96e82b11ff5210c32168b15343cd33a6bb31c20e
mostlyclicks <carlos@mostlyclicks.com>
2021-11-24 06:57:34 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/logo6.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Integrated Restorations, LLC</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site coming soon.
+        </p>
+        <p>
+          Integrated Restorations, LLC<br/>
+          228 S Park St<br/>
+          Belleville, WI 53508<br/><br/>
+
+          (608) 424-6997 (office)<br/>
+          (608) 547-1713 (mobile)
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/Integratedrestorations"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check us out on Facebook!
         </a>
       </header>
     </div>
