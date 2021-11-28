cec7831e1fc057503f6ae1791a1ccd3f6f80c14f
mostlyclicks <carlos@mostlyclicks.com>
2021-11-28 16:42:43 -0700

    add tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,11 @@ import bgImg from '../images/s_education3.jpg'
 const Header = () => {
   return (
     <header className="App-header" style={{backgroundImage: `url("${bgImg}")`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-      <div className="bg-gray-900 h-100vh w-100vw z-100 absolute border-2">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Integrated Restorations, LLC</h1>
-      </div>
+    <div className="bg-gray-900 h-100vh w-100vw z-100 absolute border-2">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>Integrated Restorations, LLC</h1>
+    </div>
+      
         
       </header>
   )
