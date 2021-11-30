be19a48c26278b369ee6270151c5196a1ec55965
mostlyclicks <carlos@mostlyclicks.com>
2021-11-29 21:12:50 -0700

    style about - stub footer

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section className=" bg-gray-700">
-    <div className="container sm:w-8/12 md:w-1/2 mx-auto py-8 px-4 text-gray-400 text-lg">
+    <section className=" bg-gray-100 bg-opacity-80">
+    <div className="container sm:w-8/12 md:w-1/2 mx-auto py-8 px-4 text-gray-500 text-lg">
       <p className="text-left mb-8"><strong>Integrated Restorations, LLC</strong> is a full-service consulting and contracting firm serving public and private lands in the Midwestern United States since 2007. Integrated was founded on the principle that successful land management must be a process-oriented, whole-system approach. Our unique approach blends the most current ecological concepts and empirical research with multiple concerted and properly sequenced practices. The use of multiple sequenced practices synergizes the natural dynamic elements of an ecological system, accelerating the recovery process to enhance habitat quality and achieve land management objectives and project targets while simultaneously reducing long-term costs.</p>
 
       <h2 className=" text-left font-bold">Integrated is THE cutting edge of restoration practices</h2>
 
-<p className="text-left mb-4">Integrated is the industry pioneer of the most innovative and scientifically rigorous habitat restoration and enhancement techniques. We designed, developed, and tested many of the theoretical approaches and standard procedures that our competition imitates. Our innovative research programs ensure that you will receive the cutting edge of the expanding science of restoration ecology. We develop and implement effective approaches to real-world problems and provide organized, affordable solutions for your land area.</p>
-      
+      <p className="text-left mb-4">Integrated is the industry pioneer of the most innovative and scientifically rigorous habitat restoration and enhancement techniques. We designed, developed, and tested many of the theoretical approaches and standard procedures that our competition imitates. Our innovative research programs ensure that you will receive the cutting edge of the expanding science of restoration ecology. We develop and implement effective approaches to real-world problems and provide organized, affordable solutions for your land area.</p>
+  
     </div>
     </section>
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div></div>
+      <div></div>
+       <p>
+          Integrated Restorations, LLC<br/>
+          228 S Park St<br/>
+          Belleville, WI 53508<br/><br/>
+
+          (608) 424-6997 (office)<br/>
+          (608) 547-1713 (mobile)
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/Integratedrestorations"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check us out on Facebook!
+        </a>
+      
+    </footer>
+  )
+}
+
+export default Footer
