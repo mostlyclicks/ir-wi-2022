edac652b42c9ae6d8512a24d21467c020857938a
mostlyclicks <carlos@mostlyclicks.com>
2021-11-29 20:28:48 -0700

    fix hero overlay and logo positioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,13 @@ import bgImg from '../images/s_education3.jpg'
 
 const Header = () => {
   return (
-    <header className="App-header" style={{backgroundImage: `url("${bgImg}")`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-    <div className="bg-gray-900 h-100vh w-100vw z-100 absolute border-2">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>Integrated Restorations, LLC</h1>
+    <header className="relative bg-no-repeat h-screen w-screen " style={{backgroundImage: `url("${bgImg}")`, backgroundSize: 'cover' }}>
+    <div className="flex flex-col justify-center relative w-full h-full z-20 justify-self-center ">
+      <img src={logo} className="self-center w-1/12 " alt="logo" />
+      <h1 className="text-3xl text-white">Integrated Restorations, LLC</h1>
     </div>
+
+    <div className="bg-green-900 opacity-40 w-full h-full absolute top-0 left-0 z-10">hello</div>
       
         
       </header>
