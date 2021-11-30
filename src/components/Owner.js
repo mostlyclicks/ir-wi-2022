f4bdaa9cc38bf0e629739fa0e6aee9935e27aff7
mostlyclicks <carlos@mostlyclicks.com>
2021-11-30 16:57:39 -0700

    add Owner section and craig image

---
+++ src/components/Owner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import CraigImg from '../images/craig-annen.jpg'
+
+const Owner = () => {
+  return (
+    <section className="flex sm:flex-row">
+      <div className="">
+        <img src={CraigImg} alt="Craig Annen" />
+      </div>
+      <div>
+
+      </div>
+
+      
+    </section>
+  )
+}
+
+export default Owner
