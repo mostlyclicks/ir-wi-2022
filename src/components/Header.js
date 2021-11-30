5d526851bb54239d302bf54ae4c0842ae7693e88
mostlyclicks <carlos@mostlyclicks.com>
2021-11-29 20:32:38 -0700

    responsize logo/h1 size

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
   return (
     <header className="relative bg-no-repeat h-screen w-screen " style={{backgroundImage: `url("${bgImg}")`, backgroundSize: 'cover' }}>
     <div className="flex flex-col justify-center relative w-full h-full z-20 justify-self-center ">
-      <img src={logo} className="self-center w-1/12 " alt="logo" />
-      <h1 className="text-3xl text-white">Integrated Restorations, LLC</h1>
+      <img src={logo} className="self-center w-1/6 sm:w-1/8 md:w-1/12 mb-8" alt="logo" />
+      <h1 className="text-2xl sm:text-4xl text-white">Integrated Restorations, LLC</h1>
     </div>
 
     <div className="bg-green-900 opacity-40 w-full h-full absolute top-0 left-0 z-10">hello</div>
