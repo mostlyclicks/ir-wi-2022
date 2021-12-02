49e41326d4855d93a087c4cc00a96c0d7b6d9218
mostlyclicks <carlos@mostlyclicks.com>
2021-12-01 21:56:29 -0700

    add/style services

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Header from './components/Header'
 import About from './components/About'
-//import Services from './components/Services'
+import Services from './components/Services'
 import Owner from './components/Owner'
 
 import Footer from './components/Footer'
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <main>
         <About />
-        
+        <Services />
         <Owner />
         
       </main>
--- src/components/Services.js
+++ src/components/Services.js
@@ -2,25 +2,31 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <section className="flex flex-col p-8 sm:flex-col md:flex-row md:flex-wrap md:justify-center">
+    <section className="flex flex-col px-8 py-16 xl:flex-row xl:flex-wrap md:justify-center lg:space-x-12 space-y-8 bg-gray-800 text-gray-200">
     
-    <h2 className="w-full">Services</h2>
-    <div className=" md:w-1/2 lg:w-60 border-2 border-gray-300">
+    <h2 className="w-full text-3xl font-bold">Services</h2>
+  
+    <div className="flex-1 ">
+      <h3 className="font-bold text-xl text-left block xl:h-14 ">Restoration Contracting</h3>
+      <p className="text-left ">Let us do the heavy lifting and dirty work. We can put your plans into motion with our broad range of contracting services. Our skill level, work ethic, and efficiency are without parallel in the industry.</p>
+    </div>
+    <div className=" flex-1 ">
+      <h3 className="font-bold text-xl text-left block xl:h-14">Habitat Enhancement for Hunting & Fishing Lands</h3>
+      <p className="text-left ">Make your hunting land more attractive to game and wildlife. Fill your freezer with venison, consistently bag your daily limit of waterfowl, hook more trout than ever, and improve safety and fun. Our wildlife ecologists are avid hunters and understand game behavior, feeding and bedding habits, and habitat preferences.</p>
+    </div>
+    <div className=" flex-1  ">
+      <h3 className="font-bold text-xl text-left block xl:h-14">Education and Outreach</h3>
+      <p className="text-left ">Land stewardship often involves the entire community. We offer affordable programs designed to supervise, train, and inform individuals of all ages and skill/experience levels on restoration topics, practices, and safety.</p>
+    </div>
+    <div className=" flex-1  ">
+      <h3 className="font-bold text-xl text-left block xl:h-14">Recreational TrailConstruction & Maintenance</h3>
+      <p className="text-left ">Getting smacked in the face by a thorny branch is unpleasant and dangerous. We will keep your recreational trails free of obstacles and fun!</p>
+    </div>
+    {/* 
+    <div className=" flex-nowrap flex-grow md:w-full lg:w-60 border-2 ">
       <h3>Consulting Services</h3>
     </div>
-    <div className=" md:w-1/2 lg:w-60 border-2 border-gray-300">
-      <h3>Restoration Contracting</h3>
-    </div>
-    <div className=" md:w-1/2 lg:w-60 border-2 border-gray-300">
-      <h3>Habitat Enhancement for Hunting & Fishing Lands</h3>
-    </div>
-    <div className=" md:w-1/2 lg:w-60 border-2 border-gray-300">
-      <h3>Education and Outreach</h3>
-    </div>
-    <div className=" md:w-1/2 lg:w-60 border-2 border-gray-300">
-      <h3>Recreational Trail Construction & Maintenan</h3>
-    </div>
-    
+    */}
       
     
     </section>
